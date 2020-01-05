ca8904cf9c1d9301e0ff02b9ae03d5f64fcaea43
ACholberton <1103@holbertonschool.com>
2020-01-05 18:53:52 +0000

    updated pall file

--- pall.c
+++ pall.c
@@ -1,12 +1,13 @@
+#include "monty.h"
 /**
- *pop - prints all the values on the stack.
+ *pall - prints all the values on the stack.
  *@list: is the stack to be printed
  *Return: the elements of the stack.
  */
 void pall(stack_t **head, unsigned int element)
 {
-	(void)elements;
-	const  stack_t stack = *head;
+	(void)element;
+	stack_t *stack = *head;
 
 	while (stack != NULL)
 	{
