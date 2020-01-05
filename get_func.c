8c892ec9d599909dc85956df3cf281e39d08b6ba
ACholberton <1103@holbertonschool.com>
2020-01-05 18:48:18 +0000

    copied get func file from test repo

---
+++ get_func.c
@@ -0,0 +1,50 @@
+#include "monty.h"
+
+/**
+ *
+ */
+
+void (*get_func(char *s, stack_t **head, unsigned int element))
+{
+	instruction_t ops[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{'\0', NULL}
+	};
+	int i = 0;
+
+	/*if (!stack)
+	  return (NULL);*/
+	while (ops[i].opcode != NULL)
+	{
+		if (strcmp(s, ops[i].opcode) == 0)
+		{
+			ops[i].f(head, element);
+		}
+		i++;
+	}
+
+	return (NULL);
+}
+
+void line_check(char *str, int line_num, stack_t **head)
+{
+	char *keyword;
+	int num = 1;
+	int j = 0;
+	(void)line_num;
+
+	keyword = strtok(str, " ");
+	j = strlen(keyword) + 1;
+	if (isdigit(str[j]))
+	{
+		num = str[j] - '0';
+	}
+	get_func(keyword, head, num);
+
+}
