205beba7d5846c503d21a1b3df15528631d09a61
ACholberton <1103@holbertonschool.com>
2020-01-05 23:08:21 +0000

    file to print top element of a stack

--- pint.c
+++ pint.c
@@ -8,28 +8,13 @@
 
 void pint(stack_t **head, unsigned int element)
 {
-	/**
-	 *char *new;
-	 new = malloc(sizeof(char));
-	 if (new = NULL)
-	 return (NULL);
-	 if (element == NULL || element == "")
-	 {
-	 printf("L<line_number>: can't pint, stack empty");
-	 exit(EXIT_FAILURE);
-	 }
-	 new->element = element;
-	 new->prev = NULL;
-	 if (*position == NULL)
-	 {
-	 *position = new;
-	 return (*position);
-	 }
-	 new->next = *position;
-	 (*position)->prev = new;
-	 *head = new;
-	 return (new);
-	 */
-	(void)head;
-	(void)element;
+	stack_t *stack;
+
+	if (*stack == NULL)
+	{
+		dprintf(STDERR_FILENO, "L<%u>: can't pint, stack empty",
+			element);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", head->n);
 }
