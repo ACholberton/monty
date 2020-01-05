58056f05ef883e230731300eca6f6633cf8b327a
waffle <asuka9767@protonmail.com>
2020-01-05 23:51:49 +0000

    new file to swap elements in list

--- swap.c
+++ swap.c
@@ -9,6 +9,17 @@
 
 void swap(stack_t **head, unsigned int element)
 {
-	(void)head;
-	(void)element;
+        int tmp1, tmp2;
+
+	if (head == NULL || *head == NULL || (*head)->next == NULL)
+	{
+		dprintf(STDERR_FILENO, "L<%u>: can't swap, stack too short",
+			element);
+		exit(EXIT_FAILURE);
+	}
+	tmp1 = (*head)->n;
+	tmp2 = (*head)->next->n;
+
+	(*head)->n = tmp2;
+	(*head)->next->n = tmp1;
 }
