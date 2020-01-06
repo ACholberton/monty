706fe7023bdf7a7082d86bf376ffacc83ef952c0
ACholberton <1103@holbertonschool.com>
2020-01-06 06:03:31 +0000

    created multipliying function

---
+++ multi.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * _mul - Entry Point
+ * @head: head of linked list
+ * @line: line num
+ * Description: multiplies the tops two elements of the stack)?
+ */
+
+void _mul(stack_t **head, unsigned int line)
+{
+	int i, j, result;
+
+	if (*head == NULL || head == NULL)
+	{
+		dprintf(STDERR_FILENO, "L<%u>: can't mul, stack too short\n",
+			line);
+		exit(EXIT_FAILURE);
+	}
+
+	i = (*head)->n;
+	j = (*head)->next->n;
+
+	result = i * j;
+
+	(*head)->n = result;
+	freeall(head);
+}
